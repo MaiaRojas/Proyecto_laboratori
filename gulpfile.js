38fa9e7d1722c588aa0c681650ba57e4e74ab98e
Maia <maia.rt.46@gmail.com>
2017-08-02 20:11:24 -0500

    Json habilitado

--- gulpfile.js
+++ gulpfile.js
@@ -1,128 +1,117 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var browserify = require('gulp-browserify');
-var rename = require('gulp-rename');
-var browserSync = require('browser-sync').create();
-// var uglify = require('gulp-uglify');
-// var toEs6 = require('gulp-6to5');
-var nodemon = require('gulp-nodemon');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserify = require('gulp-browserify');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const toEs6 = require('gulp-6to5');
+const concat = require('gulp-concat');
+const nodemon = require('gulp-nodemon');
 
-var config = {
+const config = {
     source:'./src/',
     dist:'./public/'
 };
 
-var paths = {
+const paths = {
     assets:"assets/",
     html:"**/*.html",
-    img: "img/**/*",
-    js: "js/*.js",
-
-    utils: "utils/**",
-
-    css: "css",
     sass:"scss/**/*.scss",
-    fonts: "fonts/**/*.otf",
-    mainSass:"scss/main.scss",
-    mainJS: "js/**/*.js"
+    img: "img/**/*",
+    js:"js/**/*.js",
+    fonts:"fonts/**/*.otf",
+    mainSass:"scss/main.scss"
 };
 
-var sources = {
+const sources = {
     assets:config.source + paths.assets,
     html: config.source + paths.html,
+    sass: config.source + paths.assets + paths.sass,
     img: config.source + paths.assets + paths.img,
     js:config.source + paths.assets + paths.js,
-    sass: config.source + paths.assets + paths.sass,
     fonts: config.source + paths.assets + paths.fonts,
-    rootSass: config.source + paths.assets + paths.mainSass,
-    rootJS: config.source + paths.assets + paths.mainJS
+    rootSass: config.source + paths.assets + paths.mainSass
 };
 
+
 gulp.task('html', () => {
-    gulp.src(sources.html).
-    pipe(gulp.dest(config.dist));
-});
+    gulp.src(sources.html).pipe(gulp.dest(config.dist));
 
-gulp.task('img', () => {
-    gulp.src(sources.img).
-    pipe(gulp.dest(config.dist + paths.assets + "img"));
 });
-
 gulp.task('sass', () => {
     gulp.src(sources.rootSass)
     .pipe(sass({
-        outputStyle:"expanded"
-    }).on("error",sass.logError))
-    .pipe(gulp.dest(config.dist + paths.assets + "css"));
+        outputStyle:"compressed"
+    }).on("error",sass.logError)).pipe(gulp.dest(config.dist+paths.assets+"css"));
 });
 
+gulp.task('img', () => {
+    gulp.src(sources.img).pipe(gulp.dest(config.dist+paths.assets+"img"));
+})
+
 gulp.task('js', () => {
-     gulp.src(sources.rootJS)
-        // .pipe(toEs6())
+     gulp.src(sources.js)
+        .pipe(toEs6())
         .pipe(concat('bundle.js'))
         // .pipe(uglify())
         .pipe(gulp.dest(config.dist + paths.assets +"js"));
 });
 
 gulp.task('fonts', () => {
-    gulp.src(sources.fonts).
-    pipe(gulp.dest(config.dist + paths.assets + "fonts"));
+    gulp.src(sources.fonts).pipe(gulp.dest(config.dist+paths.assets+"fonts"));
 })
 
-gulp.task("html-watch", ["html"], function (done) {
-  browserSync.reload();
-  done();
-});
-
-gulp.task("img-watch", ["img"], function (done) {
-  browserSync.reload();
-  done();
-});
-
-gulp.task("js-watch", ["js"], function (done) {
-  browserSync.reload();
-  done();
-});
-
-gulp.task("sass-watch", ["sass"], function (done) {
-  browserSync.reload();
-  done();
-});
-
-gulp.task('fonts-watch', ["fonts"], function (done) {
+gulp.task('sass-watch',["sass"], (done) => {
     browserSync.reload();
     done();
 });
 
-gulp.task('default', ['browserSync'], function () {
+gulp.task('js-watch',["js"], (done) => {
+    browserSync.reload();
+    done();
 });
 
-gulp.task('browserSync', ['nodemon'], () => {
-  browserSync.init(null, {
-    proxy: "http://localhost:3000",
-        files: ["public/**/*.*"],
-        port: 7000,
-});
+gulp.task('img-watch', ["img"], (done) =>{
+    browserSync.reload();
+    done();
+})
 
-  gulp.watch(sources.html, ["html-watch"]);
-  gulp.watch(sources.img, ["img-watch"]);
-  gulp.watch(sources.fonts, ["fonts-watch"]);
-  gulp.watch(sources.sass, ["sass-watch"]);
-  gulp.watch(sources.rootJS, ["js-watch"]);
+gulp.task('fonts-watch', ["fonts"], (done) =>{
+    browserSync.reload();
+    done();
+})
 
+gulp.task('html-watch',["html"], (done) => {
+    browserSync.reload();
+    done();
 });
 
 gulp.task('nodemon', function(cb){
-
-  const started = false;
-
-  return nodemon({
-    script: 'server.js'
-  }).on('start', function(){
-    if(!started){
+  var start = false;
+  return nodemon({script: 'server.js'}).on('start', function(){
+    if(!start){
+      start = true;
       cb();
-      started = true;
     }
   });
 });
+
+gulp.task('browser-sync', ['nodemon'],function () {
+  browserSync.init({
+       port: 7000,
+       proxy: {
+           target: "localhost:3000",
+           ws: true
+       }
+   });
+});
+
+
+gulp.task("serve", ['browser-sync'],() => {
+   gulp.watch(sources.html,["html-watch"]);
+   gulp.watch(sources.img, ["img-watch"]);
+   gulp.watch(sources.sass,["sass-watch"]);
+   gulp.watch(sources.js,["js-watch"]);
+   gulp.watch(sources.js,["fonts-watch"]);
+});
+gulp.task('run', [ 'html','sass','js', 'fonts','img','serve']);
