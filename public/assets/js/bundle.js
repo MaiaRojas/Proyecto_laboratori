b0d3aff6292311df376f0874fc3404e90a0a57c5
maia rojas <maia.rt.46@gmail.com>
2017-08-11 12:03:59 -0500

    pos

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -549,7 +549,7 @@ const getJSON = (url, cb) => {
 'use strict';
 const Postregister =(update)=>{
 
-  $.post("https://sheetsu.com/apis/v1.0/8392c0c102f9", {"Coder":state.user.Coder,"Email":state.user.Email,"Codigo":state.user.Codigo,"Squad":state.user.Squad,"Tipo":state.user.Tipo,"Dia":state.user.Dia,"Hora":state.user.Hora,"Estado":state.user.Estado,"Motivo":state.user.Motivo,"Sede":state.user.Sede}, function(result){
+  $.post("https://sheetsu.com/apis/v1.0/47e10e5a141a", {"Coder":state.user.Coder,"Email":state.user.Email,"Codigo":state.user.Codigo,"Squad":state.user.Squad,"Tipo":state.user.Tipo,"Dia":state.user.Dia,"Hora":state.user.Hora,"Estado":state.user.Estado,"Motivo":state.user.Motivo,"Sede":state.user.Sede}, function(result){
       console.log("Enviando Data");
   });
 };
--- src/assets/js/utils/postVali.js
+++ src/assets/js/utils/postVali.js
@@ -1,7 +1,7 @@
 'use strict';
 const Postregister =(update)=>{
 
-  $.post("https://sheetsu.com/apis/v1.0/8392c0c102f9", {"Coder":state.user.Coder,"Email":state.user.Email,"Codigo":state.user.Codigo,"Squad":state.user.Squad,"Tipo":state.user.Tipo,"Dia":state.user.Dia,"Hora":state.user.Hora,"Estado":state.user.Estado,"Motivo":state.user.Motivo,"Sede":state.user.Sede}, function(result){
+  $.post("https://sheetsu.com/apis/v1.0/47e10e5a141a", {"Coder":state.user.Coder,"Email":state.user.Email,"Codigo":state.user.Codigo,"Squad":state.user.Squad,"Tipo":state.user.Tipo,"Dia":state.user.Dia,"Hora":state.user.Hora,"Estado":state.user.Estado,"Motivo":state.user.Motivo,"Sede":state.user.Sede}, function(result){
       console.log("Enviando Data");
   });
 };
