85f0de47c31fd5fb6dbf5ccc9e49836c510e888b
maia rojas <maia.rt.46@gmail.com>
2017-09-24 10:10:59 -0500

    Pruebas

--- src/assets/js/components/9_Ale.js
+++ src/assets/js/components/9_Ale.js
@@ -20,7 +20,7 @@ const AlePantalla = (update) => {
 
   const coderToday = [], squads = [];
 
-  $.get("https://sheetsu.com/apis/v1.0/2049baff4052",(data)=> {
+  $.get("https://sheetsu.com/apis/v1.0/1acf2e7e65e6",(data)=> {
     console.log(data);
     state.asistencia = data;
 
